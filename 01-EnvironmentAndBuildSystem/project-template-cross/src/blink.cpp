5cc129bf951ddb028042e1c91a17bacf83823038
Angela <nikodinovskaa4@hotmail.com>
2022-07-16 22:09:50 +0200

    Fixes in the STM32 CMake template blink example

--- 01-EnvironmentAndBuildSystem/project-template-cross/src/blink.cpp
+++ 01-EnvironmentAndBuildSystem/project-template-cross/src/blink.cpp
@@ -7,14 +7,14 @@ int main()
 {
   constexpr uint8_t ledPin = 13;  //< LED on PC13
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; //< clock for GPIOC
-  GPIOE->MODER = 0x1 << (2*ledPin);  //< general purpose output mode
+  GPIOC->MODER = 0x1 << (2*ledPin);  //< general purpose output mode
 
   while(true)
   {
-    GPIOE->ODR |= 1 << ledPin;
-    for(uint32_t i = 0; i < 10000000; ++i);  //< some delay
-    GPIOE->ODR &= ~(1 << ledPin);
-    for(uint32_t i = 0; i < 10000000; ++i);  //< some delay
+    GPIOC->ODR |= 1 << ledPin;
+    for(uint32_t i = 0; i < 1000000; ++i);  //< some delay
+    GPIOC->ODR &= ~(1 << ledPin);
+    for(uint32_t i = 0; i < 1000000; ++i);  //< some delay
   }
 
   return 0;
