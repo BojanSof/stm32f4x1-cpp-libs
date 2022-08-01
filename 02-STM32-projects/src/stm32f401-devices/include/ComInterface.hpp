8c2a94c53ad308f596ea6467933cbc9a6b6f5b83
Angela <nikodinovskaa4@hotmail.com>
2022-08-01 13:37:56 +0200

    Added ComInterface base class.

---
+++ 02-STM32-projects/src/stm32f401-devices/include/ComInterface.hpp
@@ -0,0 +1,42 @@
+#ifndef COM_INTERFACE_HPP
+#define COM_INTERFACE_HPP
+
+#include <cstddef>
+#include <functional>
+
+namespace Stm32
+{
+
+  struct ComConfig {};
+
+  class ComInterface
+  {
+    public:
+      using CallbackT = std::function<void()>;
+      
+      virtual void configure(const ComConfig& config) = 0;
+
+      virtual void asyncRead(std::byte * const buffer, const size_t bufferSize, size_t& readSize, const CallbackT& callback) = 0;
+      virtual void asyncWrite(const std::byte * const buffer, const size_t bufferSize, size_t& writeSize, const CallbackT& callback) = 0;
+
+      size_t read(std::byte * const buffer, const size_t bufferSize)
+      {
+        size_t readSize = 0;
+        volatile bool readDone = false;
+        asyncRead(buffer, bufferSize, readSize, [&readDone](){ readDone = true; });
+        while(!readDone);
+        return readSize;
+      }
+
+      size_t write(const std::byte * const buffer, const size_t bufferSize)
+      {
+        size_t writeSize = 0;
+        volatile bool writeDone = false;
+        asyncWrite(buffer, bufferSize, writeSize, [&writeDone](){ writeDone = true; });
+        while(!writeDone);
+        return writeSize;
+      }
+  };
+
+}
+#endif  //COM_INTERFACE_HPP
