8334736c72404374927583e0a2f3b5bd54812d5d
Angela Nikodinovska <nikodinovskaa4@hotmail.com>
2022-10-11 20:29:51 +0200

    WIP Adding tinyusb cmake target.

---
+++ cmake/FindTinyusb.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  Tinyusb
+  GIT_REPOSITORY https://github.com/hathach/tinyusb.git
+  GIT_TAG        "0.14.0"
+  GIT_SUBMODULES "hw/mcu/st/cmsis_device_f4" "hw/mcu/st/stm32f4xx_hal_driver"
+  GIT_SHALLOW    TRUE
+  GIT_PROGRESS   TRUE
+)
+
+FetchContent_MakeAvailable(Tinyusb)
+write_file(${tinyusb_SOURCE_DIR}/CMakeLists.txt "include(${CMAKE_CURRENT_LIST_DIR}/tinyusb.cmake)")
+add_subdirectory(${tinyusb_SOURCE_DIR})
---
+++ cmake/tinyusb.cmake
@@ -0,0 +1,27 @@
+project(Tinyusb LANGUAGES C ASM)
+
+add_library(Tinyusb)
+
+set_target_properties(Tinyusb
+  PROPERTIES
+    C_STANDARD 11
+)
+
+target_include_directories(Tinyusb
+  PUBLIC
+    "${CMAKE_CURRENT_SOURCES}/src"
+    "${CMAKE_CURRENT_LIST_DIR}"
+)
+
+target_sources(Tinyusb
+  PRIVATE
+    "src/tusb.c"
+    "src/class/cdc/cdc_device.c"
+    "src/portable/synopsys/dwc2/dcd_dwc2.c"
+)
+
+target_link_libraries(Tinyusb
+  PRIVATE
+    ${CMSIS_LIBRARY}
+    ${HAL_LIBRARY}
+)
