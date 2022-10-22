bb533fead25c287b981134fae2c1f2d32e5c02f3
Bojan Sofronievski <bojan.drago@gmail.com>
2022-10-22 16:58:56 +0200

    Guess the number for `SerialUsb` testing.

--- src/serial-usb-example/SerialUsbExample.cpp
+++ src/serial-usb-example/SerialUsbExample.cpp
@@ -1,138 +1,93 @@
 #include <cstdint>
+#include <charconv>
 #include <chrono>
 #include <string_view>
 
 #include <STM32F4x1/SerialUsb.hpp>
 #include <STM32F4x1/CycleCounter.hpp>
 
-#include <FsmGenerator/FsmGenerator.hpp>
-
-using namespace Fsm;
-
-class GuessNumberFsm;
-
-template<>
-struct Fsm::States<GuessNumberFsm>
-{
-  enum class State
-  {
-    Greet,
-    InvalidState,
-    WaitForInput,
-    CheckInput,
-    End
-  };
-};
-
-class GuessNumberFsm
-  : public FsmGenerator<GuessNumberFsm>
+class GuessNumber
 {
   public:
-    using State = typename States<GuessNumberFsm>::State;
-
-    GuessNumberFsm(Stm32::SerialUsb& serial)
-      : FsmGenerator{State::CheckInput}
-      , running{true}
+    GuessNumber(Stm32::SerialUsb& serial)
+      : running{true}
+      , firstRun{true}
       , serial_{serial}
     {
       num = 12345;
-      processEvent(GreetEvent{});
     }
 
     void run()
     {
-      if(running)
+      using namespace std::literals;
+      if(!running) return;
+      if(firstRun)
       {
-        processEvent(RetryEvent{});
-        InputEvent in;
-        in.number = input;
-        processEvent(in);
-        if(!running) processEvent(EndEvent{});
+        Stm32::CycleCounter::delay(8s);
+        printString("Welcome to Guess the Number!\r\n"sv);
+        firstRun = false;
       }
-    }
-  private:
-    uint16_t num, input;
-    bool running;
-    Stm32::SerialUsb& serial_;
-  private:
-    friend class FsmGenerator<GuessNumberFsm>;
 
-    struct GreetEvent : public Event {};
-    struct InputEvent : public Event
-    {
-      unsigned int number;
-    };
-    struct RetryEvent : public Event {};
-    struct EndEvent : public Event {};
-
-    void doGreetCheckInput(const GreetEvent& e)
-    {
-      (void)e;
-      static constexpr std::string_view msg{"Welcome to Guess the number."};
-      using namespace std::chrono_literals;
-      serial_.write(
-          reinterpret_cast<const std::byte*>(msg.data())
-        , msg.size()
-        , 1s
-      );
-    }
-
-    void doWaitForInputCheckInput(const InputEvent& e)
-    {
-      if(e.number != num)
+      if(getInput())
       {
-        using namespace std::chrono_literals;
-        const std::string_view msg{(e.number < num) ? "Low, enter higher number." : "High, enter lower number."};
-        serial_.write(
-            reinterpret_cast<const std::byte*>(msg.data())
-          , msg.size()
-          , 1s
-        );
+        if(input != num)
+        {
+          printString((input < num)
+                      ? "Low, enter higher number.\r\n"sv
+                      : "High, enter lower number.\r\n"sv);
+        }
+        else
+        {
+          printString("Congrats! Bye.\r\n"sv);
+          running = false;
+        }
       }
       else
       {
-        running = false;
-      }
+        printString("\r\nPlease give valid input.\r\n"sv);
+      }      
     }
 
-    void doCheckInputWaitForInput(const RetryEvent& e)
+    void reset()
     {
-      (void)e;
+      firstRun = true;
+      running = true;
+    }
+
+    bool isRunning() const { return running; }
+  private:
+    void printString(const std::string_view str)
+    {
       using namespace std::chrono_literals;
-      std::string_view msg{"Enter number between 0 and 65535: "};
       serial_.write(
-          reinterpret_cast<const std::byte*>(msg.data())
-        , msg.size()
+          reinterpret_cast<const std::byte*>(str.data())
+        , str.size()
         , 1s
       );
-      static constexpr std::string_view expected{"\n"};
-      serial_.read(
-          reinterpret_cast<std::byte*>(&input)
-        , sizeof(input)
+    }
+
+    bool getInput()
+    {
+      using namespace std::literals;
+      printString("Enter number between 0 and 65535: "sv);
+
+      static constexpr std::string_view expected{"\r\n"};
+      char inputStr[10]{};
+      size_t numRead = serial_.read(
+          reinterpret_cast<std::byte*>(inputStr)
+        , sizeof(inputStr)
         , reinterpret_cast<const std::byte*>(expected.data())
         , expected.size()
         , 10s
       );
+      if(numRead < expected.size()) return false;
+      const auto [ptr, ec] = std::from_chars(inputStr, inputStr + numRead - expected.size(), input);
+      return (ec == std::errc());
     }
-
-    void doCheckInputStop(const EndEvent& e)
-    {
-      (void)e;
-      using namespace std::chrono_literals;
-      static constexpr std::string_view msg{"Congrats! Bye."};
-      serial_.write(
-          reinterpret_cast<const std::byte*>(msg.data())
-        , msg.size()
-        , 1s
-      );
-    }
-
-    using Table = TransitionTable<
-        Transition<GreetEvent, State::Greet, State::CheckInput, &GuessNumberFsm::doGreetCheckInput>
-      , Transition<InputEvent, State::WaitForInput, State::CheckInput, &GuessNumberFsm::doWaitForInputCheckInput>
-      , Transition<RetryEvent, State::CheckInput, State::WaitForInput, &GuessNumberFsm::doCheckInputWaitForInput>
-      , Transition<EndEvent, State::CheckInput, State::End, &GuessNumberFsm::doCheckInputStop>
-    >;
+  private:
+    uint16_t num, input;
+    bool running, firstRun;
+    Stm32::SerialUsb& serial_;
 };
 
 int main()
@@ -142,14 +97,18 @@ int main()
 
   decltype(auto) serial = SerialUsb::getInstance();
   
-  GuessNumberFsm guessNumber{serial};
+  GuessNumber guessNumber{serial};
 
   while(true)
   {
+    serial.update();
     if(serial.connected())
     {
       guessNumber.run();
     }
-    serial.update();
+    else
+    {
+      guessNumber.reset();
+    }
   }
 }
