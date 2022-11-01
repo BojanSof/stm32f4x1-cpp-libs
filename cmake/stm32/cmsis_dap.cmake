975772b93725a1337795ce370ee7fb4bb33bc4e1
Bojan Sofronievski <bojan.drago@gmail.com>
2022-11-01 21:51:58 +0100

    Fixed typo in `cmsis_dap` targets names.

--- cmake/stm32/cmsis_dap.cmake
+++ cmake/stm32/cmsis_dap.cmake
@@ -1,7 +1,7 @@
 function(cmsis_dap_flash TARGET)
 	set(BINARY ${TARGET}.bin)
 	set(FLASH_START "0x08000000")
-	add_custom_target(${TARGET}-cmsis_cap-flash 
+	add_custom_target(${TARGET}-cmsis_dap-flash 
 		COMMAND openocd -f ${PROJECT_SOURCE_DIR}/cmake/stm32/openocd.cfg
 							-c "init"
               -c "reset halt"
