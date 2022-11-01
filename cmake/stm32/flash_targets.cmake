b3c2e75244f1931c60cadfa718e3fa634bfe3697
Bojan Sofronievski <bojan.drago@gmail.com>
2022-11-01 21:24:55 +0100

    Added CMSIS-DAP flash target.

--- cmake/stm32/flash_targets.cmake
+++ cmake/stm32/flash_targets.cmake
@@ -1,7 +1,9 @@
 include(${PROJECT_SOURCE_DIR}/cmake/stm32/stlink.cmake)
 include(${PROJECT_SOURCE_DIR}/cmake/stm32/jlink.cmake)
+include(${PROJECT_SOURCE_DIR}/cmake/stm32/cmsis_dap.cmake)
 
 function(generate_flash_targets TARGET)
   st_link_flash(${TARGET})
   jlink_flash(${TARGET})
+  cmsis_dap_flash(${TARGET})
 endfunction()
